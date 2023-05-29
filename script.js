b2f1e7a78ff42b3ea7e73e335f8c460899dd3195
Vutnur Nethaji Goud <130832771+VUTNUR@users.noreply.github.com>
2023-05-29 17:52:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 window.promises = [];
 
-// Do not change the code above this
-// add your promises to the array `promises`
+// Function to generate a random delay between 1 and 5 seconds
+function getRandomDelay() {
+  return Math.floor(Math.random() * 5000) + 1000; // Random number between 1000 and 5000 milliseconds
+}
+
+// Create five promises with random delays
+for (let i = 0; i < 5; i++) {
+  const promise = new Promise((resolve) => {
+    const delay = getRandomDelay();
+    setTimeout(() => {
+      resolve(`Promise ${i + 1} resolved after ${delay}ms`);
+    }, delay);
+  });
+  window.promises.push(promise);
+}
+
+// Use Promise.any() to wait for the first promise to resolve
+Promise.any(window.promises)
+  .then((result) => {
+    const outputDiv = document.getElementById("output");
+    outputDiv.textContent = result;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
